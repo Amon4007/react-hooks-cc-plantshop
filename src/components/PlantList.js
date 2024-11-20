3273ded5a7fab4aeabd531e1eff4d19c484a2181
amon <amonlimo4@gmail.com>
2024-11-20 22:28:17 +0300

    adds PlantList.js

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,19 +2,26 @@ import React, { useEffect } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList({ plants, setPlants, searchedPlant }) {
-  useEffect(() => {
-    // Add any necessary effect logic here, if applicable
-  }, [plants]);
 
-  const filteredPlants = plants.filter((plant) =>
-    plant.name.toLowerCase().includes(searchedPlant.toLowerCase())
-  );
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(data => setPlants(data))
+  }, [])
+
+  const filteredPlants = searchedPlant === "" ?
+    [...plants] :
+    plants.filter(plant => plant.name.toLowerCase().includes(searchedPlant.toLowerCase()))
+
+  function renderPlants() {
+    return filteredPlants.map(plant => {
+      return <PlantCard key={plant.id} plant={plant}/>
+    })
+  }
 
   return (
-    <ul className="plant-list">
-      {filteredPlants.map((plant) => (
-        <PlantCard key={plant.id} plant={plant} />
-      ))}
+    <ul className="cards">
+      {renderPlants()}
     </ul>
   );
 }
