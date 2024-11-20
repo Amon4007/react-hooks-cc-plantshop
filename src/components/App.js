37045f5550d3438fa1c05814f54f8cfa1398ea30
amon <amonlimo4@gmail.com>
2024-11-20 21:55:06 +0300

    adds App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,3 +12,17 @@ function App() {
 }
 
 export default App;
+import React from "react";
+import Header from "./Header";
+import PlantPage from "./PlantPage";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <PlantPage />
+    </div>
+  );
+}
+
+export default App;
